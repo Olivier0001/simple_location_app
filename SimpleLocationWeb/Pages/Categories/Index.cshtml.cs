0f4f7e784a7639e744da154a3ac1f05576306114
Olivier MALAMOU <olivier.malamou@syd.fr>
2023-03-16 11:43:22 +0100

    Création de la page Category avec le tableau et le bouton Creer une category

--- SimpleLocationWeb/Pages/Categories/Index.cshtml.cs
+++ SimpleLocationWeb/Pages/Categories/Index.cshtml.cs
@@ -7,16 +7,16 @@ namespace SimpleLocationWeb.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        //private readonly ApplicationDbContext _db;
-        //public IEnumerable<Category> Categories { get; set; }
-        //public IndexModel(ApplicationDbContext db)
-        //{
-        //    _db = db;
-        //}
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
-            //Categories = _db.Category;
+            Categories = _db.Category;
         }
     }
 }
